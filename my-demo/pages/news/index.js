f519c66037401a7de00695ff8159f114b25df6e6
Peto Gurin <no.mi83@yahoo.com.vn>
2023-07-31 22:01:15 +0700

    Link between pages by Link components

--- my-demo/pages/news/index.js
+++ my-demo/pages/news/index.js
@@ -1,7 +1,13 @@
-
+import Link from "next/link";
 
 export default function NewsPage() {
   return (
-    <div>news</div>
-  )
+    <>
+      <h1>The news</h1>
+      <ul>
+        <li><Link href="/news/nextjs">NextJs Is a Great Framework</Link></li>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
 }
